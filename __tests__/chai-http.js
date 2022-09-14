037d66d0764e2b5a129e30ee188498b6a666a92c
Andreas Benckert <andreas.benckert@gmail.com>
2022-09-14 09:46:50 +0200

    Test Get active

--- __tests__/chai-http.js
+++ __tests__/chai-http.js
@@ -5,25 +5,21 @@ chai.use(chaiHttp);
 
 const HOST = process.env.HOST || "http://localhost:4000/users"
 
-
-
-
-// describe("Testing right2drive with chaitest", () => {
-// 	describe('/get all', () => {
-// 		it('should get all the users', (done) => {
-//         chai.request(HOST)
-// 			.get('/')
-//             .end((err, res) => {
-//                 expect(res).to.have.status(200)
-//                 expect(res.body).to.not.be.null
-//                 expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-//                 expect(res).to.be.json;
-//                 done()
-//             })
-// 		})
-// 	})
-// })
-
+describe("Testing right2drive with chaitest", () => {
+	describe('/get all', () => {
+		it('should get all the users', (done) => {
+        chai.request(HOST)
+			.get('/')
+            .end((err, res) => {
+                expect(res).to.have.status(200)
+                expect(res.body).to.not.be.null
+                expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                expect(res).to.be.json;
+                done()
+            })
+		})
+	})
+})
 
 const newUsers = {
 	"category": {
